b2a1cd5e37eb019e2649ee111587ecb7fb304d03
Lipkowsky <piotr.lipkowsky@gmail.com>
2024-06-18 12:09:54 +0200

    PUSH FIX

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,18 @@
-import { authMiddleware } from "@clerk/nextjs";
- 
-export default authMiddleware({
-  publicRoutes: [
-    '/',
-    '/events/:id',
-    '/api/webhook/clerk',
-    '/api/webhook/stripe',
-    '/api/uploadthing'
-  ],
-  ignoredRoutes: [
-    '/api/webhook/clerk',
-    '/api/webhook/stripe',
-    '/api/uploadthing'
-  ]
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+
+const isProtectedRoute = createRouteMatcher([
+  // '/',
+  // '/events/:id',
+  // '/api/webhook/clerk',
+  // '/api/webhook/stripe',
+  // '/api/uploadthing',
+]);
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
 });
- 
+
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
- 
