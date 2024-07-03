002a572862ead89be174b6fe9e2d9a7dcaa07698
Lipkowsky <piotr.lipkowsky@gmail.com>
2024-07-03 13:08:42 +0200

    search WP

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,15 +1,22 @@
 import Collection from "@/components/shared/Collection";
 import { Button } from "@/components/ui/button";
 import { getEventsByUser } from "@/lib/actions/event.actions";
+import { getOrdersByUser } from "@/lib/actions/order.actions";
+import { IOrder } from "@/lib/database/models/order.model";
+import { SearchParamProps } from "@/types";
 import { auth } from "@clerk/nextjs/server";
 import Link from "next/link";
 import React from "react";
 
-const ProfilePage = async () => {
+const ProfilePage = async ({ searchParams }: SearchParamProps) => {
   const { sessionClaims } = auth();
   const userId = sessionClaims?.userId as string;
 
-const organizedEvents = await getEventsByUser({userId, page: 1});
+  const ordersPage = Number(searchParams?.ordersPage || 1);
+  const eventsPage = Number(searchParams?.eventsPage || 1);
+  const orders = await getOrdersByUser({ userId, page: ordersPage });
+  const orderedEvents = orders?.data.map((order: IOrder) => order.event || []);
+  const organizedEvents = await getEventsByUser({ userId, page: eventsPage });
 
   return (
     <>
@@ -23,16 +30,16 @@ const organizedEvents = await getEventsByUser({userId, page: 1});
         </div>
       </section>
       <section className="wrapper my-8">
-        {/* <Collection 
-          data={}
+        <Collection
+          data={orderedEvents}
           emptyTitle="No Event tickets purchased yet"
           emptyStateSubtext="No worries - plenty of exciting evets to explore"
           collectionType="My_Tickets"
           limit={3}
-          page={1}
-          totalPages={2}
+          page={ordersPage}
+          totalPages={orders?.totalPages}
           urlParamName="ordersPage"
-        /> */}
+        />
       </section>
 
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
@@ -45,19 +52,19 @@ const organizedEvents = await getEventsByUser({userId, page: 1});
       </section>
 
       <section className="wrapper my-8">
-        <Collection 
+        <Collection
           data={organizedEvents?.data}
           emptyTitle="No Events have been created yet"
           emptyStateSubtext="Go create some now"
           collectionType="Events_Organized"
           limit={6}
-          page={1}
-          totalPages={2}
+          page={eventsPage}
+          totalPages={organizedEvents?.totalPages}
           urlParamName="eventsPage"
         />
       </section>
     </>
   );
-}
+};
 
 export default ProfilePage;
