4f430fe2576b44dfe4d26f1cff9576ed61eb840b
Lipkowsky <piotr.lipkowsky@gmail.com>
2024-07-04 11:42:51 +0200

    fixed category search

--- components/shared/CategoryFilter.tsx
+++ components/shared/CategoryFilter.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import {
   Select,
@@ -6,7 +6,7 @@ import {
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "@/components/ui/select"
+} from "@/components/ui/select";
 import { getAllCategories } from "@/lib/actions/category.actions";
 import { ICategory } from "@/lib/database/models/category.model";
 import { formUrlQuery, removeKeysFromQuery } from "@/lib/utils";
@@ -17,35 +17,40 @@ const CategoryFilter = () => {
   const [categories, setCategories] = useState<ICategory[]>([]);
   const router = useRouter();
   const searchParams = useSearchParams();
+  const [prevCategory, setPrevCategory] = useState<string | null>(null);
+
+  useEffect(() => {
+    const currentCategory = searchParams.get("category");
+    if (currentCategory) {
+      setPrevCategory(currentCategory);
+    }
+  }, [searchParams]);
 
   useEffect(() => {
     const getCategories = async () => {
       const categoryList = await getAllCategories();
-
-      categoryList && setCategories(categoryList as ICategory[])
-    }
+      categoryList && setCategories(categoryList as ICategory[]);
+    };
 
     getCategories();
-  }, [])
+  }, []);
 
   const onSelectCategory = (category: string) => {
-      let newUrl = '';
+    let newUrlParams = new URLSearchParams(searchParams.toString());
 
-      if(category && category !== 'All') {
-        newUrl = formUrlQuery({
-          params: searchParams.toString(),
-          key: 'category',
-          value: category
-        })
-      } else {
-        newUrl = removeKeysFromQuery({
-          params: searchParams.toString(),
-          keysToRemove: ['category']
-        })
-      }
+    if (category && category !== "All") {
+      newUrlParams.set("category", category);
+    } else {
+      newUrlParams.delete("category");
+    }
 
-      router.push(newUrl, { scroll: false });
-  }
+    if (prevCategory !== category) {
+      newUrlParams.set("page", "1");
+    }
+
+    setPrevCategory(category);
+    router.push(`/?${newUrlParams.toString()}`, { scroll: false });
+  };
 
   return (
     <Select onValueChange={(value: string) => onSelectCategory(value)}>
@@ -53,16 +58,22 @@ const CategoryFilter = () => {
         <SelectValue placeholder="Category" />
       </SelectTrigger>
       <SelectContent>
-        <SelectItem value="All" className="select-item p-regular-14">All</SelectItem>
+        <SelectItem value="All" className="select-item p-regular-14">
+          All
+        </SelectItem>
 
         {categories.map((category) => (
-          <SelectItem value={category.name} key={category._id} className="select-item p-regular-14">
+          <SelectItem
+            value={category.name}
+            key={category._id}
+            className="select-item p-regular-14"
+          >
             {category.name}
           </SelectItem>
         ))}
       </SelectContent>
     </Select>
-  )
-}
+  );
+};
 
-export default CategoryFilter
+export default CategoryFilter;
